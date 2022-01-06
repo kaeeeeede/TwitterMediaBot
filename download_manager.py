374c15b8be5c7899b63d4af4c6612bd6f3d7cf06
Anthony Wong <antawck@gmail.com>
2022-01-07 04:25:17 +0800

    returns filepath

--- download_manager.py
+++ download_manager.py
@@ -1,6 +1,13 @@
 import youtube_dl
 import shutil
 
+latest_download_destination = ""
+
+def finish_hook(s):
+	if s['status'] == 'finished':
+		global latest_download_destination
+		latest_download_destination = s['filename']
+
 def downloadMedia(url, filename = "Downloads/temp", sizeLimit = "8M", cleanupBeforeDownloading = True):
 
 	if cleanupBeforeDownloading:
@@ -8,11 +15,14 @@ def downloadMedia(url, filename = "Downloads/temp", sizeLimit = "8M", cleanupBef
 
 	ydl_opts = {
 		'outtmpl' : f'{filename}.%(ext)s',
-		'format' : f'best'
+		'format' : f'best',
+		'progress_hooks' : [finish_hook]
 	}
 
 	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-	    ydl.download([url])	
+	    print(ydl.download([url]))	 
+
+	return latest_download_destination   
 
 def cleanup(folder = "Downloads"):
-	shutil.rmtree(folder, ignore_errors = True)		
+	shutil.rmtree(folder, ignore_errors = True)
