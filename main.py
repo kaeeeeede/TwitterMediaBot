fae8e3500628a13634517ff458976321af826c1e
kaeeeeede <kaedewrites1@gmail.com>
2022-01-07 01:10:03 +0800

    modified main, added testPic

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
 
 import discord
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_choice, create_option
 from dotenv import load_dotenv
 
+#To get the bot online
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+#Test command
+bot = commands.Bot(command_prefix="!")
+slash = SlashCommand(bot, sync_commands=True)
 
-@client.event
-async def on_ready():
-	print(f'{client.user} has connected to Discord!')
+@slash.slash(
+	name="test",
+	description="Sends a test image"
+)
 
-client.run(TOKEN)
+async def test(ctx:SlashContext):
+	await ctx.send(file=discord.File('testPic.png'))
+
+bot.run(TOKEN)
