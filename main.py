c93cc26f048360d5df0a5ca60f6db30cb1da520c
Anthony Wong <antawck@gmail.com>
2022-01-07 16:45:58 +0800

    reports size of file

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ async def linkMedia(ctx:SlashContext, address):
 		await ctx.send(file=discord.File(path))
 
 	except discord.errors.HTTPException as e:
-		await ctx.send("The absolute unit of a file was way too large for Discord to handle. We may or may not handle this with file hosting services in the near or far future.")
+		filesize = format_byte_to_megabyte(get_filesize(path))
+		await ctx.send(f'The absolute unit of a file was way too large ({filesize} MB) for Discord to handle. We may or may not handle this with file hosting services in the near or far future.')
 
 	except HTTPError as e:
 		await ctx.send("Could not establish a connection.")
@@ -48,10 +49,15 @@ async def linkMedia(ctx:SlashContext, address):
 		await ctx.send("Something unexpected went wrong. Trying again will likely not help, but feel free to do so.")
 		raise e
 
-
 @bot.event
 async def on_ready():
 	print("Ready!")
+
+def get_filesize(path):
+	return os.path.getsize(path)
+
+def format_byte_to_megabyte(byte):
+	return "{:.2f}".format(byte/1000/1000)
 	
 bot.run(TOKEN)
 
