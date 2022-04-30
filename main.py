2129ae37cd25fa8690fba42791c30d40baf3ab6c
Anthony Wong <antawck@gmail.com>
2022-05-01 04:58:17 +0800

    migrated to hikari v1

--- main.py
+++ main.py
@@ -3,13 +3,11 @@ import download_manager
 from db import db
 import datetime
 
-import discord
-from discord.ext import commands
-from discord_slash import SlashCommand, SlashContext
-from discord_slash.utils.manage_commands import create_option
-
 import lightbulb
 from hikari.presences import Status, Activity
+import hikari.errors
+from hikari.messages import MessageFlag
+from hikari.interactions import ResponseType
 
 from dotenv import load_dotenv
 
@@ -19,66 +17,43 @@ download_timeout = int(os.getenv('DOWNLOAD_TIMEOUT'))
 
 download_manager.max_download_duration = download_timeout
 
-#bot = commands.Bot(command_prefix="!", activity=discord.Activity(type=discord.ActivityType.watching, name='the chat'))
 bot = lightbulb.BotApp(token = TOKEN)
-#slash = SlashCommand(bot, sync_commands=True)
 
 report_guild_ids = [int(server_id) for server_id in os.getenv('REPORT_SERVER_IDS').split(",")]
 
-# @slash.slash(
-# 	name="linkMedia",
-# 	description="Takes a URL",
-# 	options=[
-# 		create_option(
-# 			name="address",
-# 			description="Enter a URL",
-# 			required=True,
-# 			option_type=3,
-# 		)
-# 	],
-# )
 
 @bot.command
 @lightbulb.option("address", "Enter a URL")
-@lightbulb.command("linkmedia", "Downloads (video) media from the specified URL", ephemeral = True, auto_defer = True)
+@lightbulb.command("linkmedia", "Downloads (video) media from the specified URL", ephemeral = False, auto_defer = False)
 @lightbulb.implements(lightbulb.SlashCommand)
 
 async def linkMedia(ctx):
 	try:
+		await ctx.respond("hold up", flags = MessageFlag.EPHEMERAL)
 		await bot.update_presence(status = Status.DO_NOT_DISTURB, activity = Activity(name = "dead or is busy", type = 0))
 	
 		path = download_manager.downloadMedia(ctx.options.address)
 		filesize_bytes = get_filesize(path)
 		filesize_mb = format_byte_to_megabyte(filesize_bytes)
+		
+		await ctx.respond(attachment = path)
 
-		print(dir(ctx))
+		db.execute("INSERT INTO interactions (datetime, url, size) VALUES (?, ?, ?)", (datetime.datetime.now(), filepathToUrl(path), filesize_bytes))		
+		db.commit()		
 
-	# 	if not (target := ctx.channel):
-	# 		target = ctx.author
+	except hikari.errors.ClientHTTPResponseError as e:
+		await ctx.respond(f'The absolute unit of a file was way too large ({filesize_mb} MB) for Discord to handle.', flags = MessageFlag.EPHEMERAL)
 
-	# 	await target.send(file=discord.File(path))
-	# 	await ctx.send("Done!", hidden = True)
+	except RuntimeError as e:
+		await ctx.respond("The file took too long to download.", flags = MessageFlag.EPHEMERAL)
 
-	# 	db.execute("INSERT INTO interactions (datetime, url, size) VALUES (?, ?, ?)", (datetime.datetime.now(), filepathToUrl(path), filesize_bytes))		
-	# 	db.commit()		
+	except BaseException as e:
+		await ctx.respond("Something unexpected went wrong. Trying again will likely not help, but feel free to do so.", flags = MessageFlag.EPHEMERAL)
+		raise e
 
-	# except discord.errors.HTTPException as e:
-	# 	await ctx.send(f'The absolute unit of a file was way too large ({filesize_mb} MB) for Discord to handle.', hidden = True)
-
-	# except HTTPError as e:
-	# 	await ctx.send("Could not establish a connection.", hidden = True)
-
-	# except RuntimeError as e:
-	# 	await ctx.send("The file took too long to download.", hidden = True)
-
-	# except BaseException as e:
-	# 	await ctx.send("Something unexpected went wrong. Trying again will likely not help, but feel free to do so.", hidden = True)
-	# 	raise e
-
-	# finally:
-	# 	await bot.change_presence(status=discord.Status.online, activity=discord.Activity(type=discord.ActivityType.watching, name='the chat'))
 	finally:
-		pass
+		await bot.update_presence(status = Status.DO_NOT_DISTURB, activity = Activity(type = 3, name='the chat'))
+
 
 def get_filesize(path):
 	return os.path.getsize(path)
